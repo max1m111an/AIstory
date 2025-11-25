d0f76563df8abac7523f3a8b6201e1cdbad8b8ae
Begun0vM <maksimilianbegunov@inbox.ru>
2025-11-26 01:08:47 +0300

    database/models: Add events
    periods, eras
    change for mapped (sqla2+)
    rename User -> UserModel

--- database/models/user.py
+++ database/models/user.py
@@ -1,12 +1,22 @@
-from sqlalchemy import Column, Integer, String, DateTime
+import datetime
+
+from sqlalchemy import String, DateTime, UniqueConstraint
+from sqlalchemy.orm import Mapped
 from sqlalchemy.sql import func
+from sqlalchemy.orm import mapped_column
+
 from database import Base
 
 
-class User(Base):
+class UserModel(Base):
+    uid = (
+        "username",
+        "telegram_id",
+    )
     __tablename__ = "users"
+    __table_args__ = (UniqueConstraint(*uid),)
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(100), unique=True, index=True)
-    telegram_id = Column(Integer, unique=True, index=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(100))
+    telegram_id: Mapped[int] = mapped_column()
+    created_at: Mapped[datetime.date] = mapped_column(DateTime(timezone=True), server_default=func.now())
