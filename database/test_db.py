ac21c580b52d173f2463cfb1552b65d813b6230e
Begun0vM <maksimilianbegunov@inbox.ru>
2025-11-26 01:09:05 +0300

    database/test_db: fix

--- database/test_db.py
+++ database/test_db.py
@@ -1,28 +1,22 @@
 import asyncio
 
 from dotenv import load_dotenv
-from sqlalchemy import select
 from database import database
-from database.models.user import User
+from database.models.user import UserModel
 
 load_dotenv(".env") or load_dotenv("../.env")
 
 async def test_db():
-    await database.init()
-
     async with database.session() as session:
-        test_user = User(
+        test_user = UserModel(
             username="test_user",
             telegram_id=123456
         )
         session.add(test_user)
         await session.commit()
+        await session.refresh()
 
-        stmt = select(User).where(User.username == "test_user")
-        result = await session.execute(stmt)
-        user = result.scalar()
-
-        print(f"User created: {user.username}, ID: {user.id}")
+        print(f"UserModel created: {test_user.username}, ID: {test_user.id}")
 
 
 if __name__ == "__main__":
