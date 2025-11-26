c57caca6b00e7467652e26dd073b4644d0981e3d
Begun0vM <maksimilianbegunov@inbox.ru>
2025-11-26 20:57:33 +0300

    main: add load data to db by xlsx file

--- apps/main.py
+++ apps/main.py
@@ -1,9 +1,12 @@
 import os
 import asyncio
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from io import BytesIO
 
-from database.database import database
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+from database import load_data_to_db
+from database.db_engine import database
 from database.models.user import UserModel
 
 
@@ -38,6 +41,25 @@ async def test_db_command(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
         await update.message.reply_text(f'❌ Ошибка БД: {str(e)}')
 
 
+async def load_datafile_to_db(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    document = update.message.document
+    print(document.file_name)
+    await update.message.reply_text(f"File: {document.file_name}, Size: {document.file_size}")
+
+    if document.mime_type not in ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                              'application/vnd.ms-excel']:
+        await update.message.reply_text(f'Wrong file extension.')
+
+    file = await document.get_file()
+    bio = BytesIO()
+    await file.download_to_memory(bio)
+    try:
+        rows_count = await load_data_to_db(bio)
+        await update.message.reply_text(f'Loaded {rows_count} rows')
+    except Exception as e:
+        await update.message.reply_text(f'load_datafile err: {str(e)}')
+
+
 def main():
     loop = asyncio.get_event_loop()
     loop.run_until_complete(database.init())
@@ -47,6 +69,7 @@ def main():
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("testdb", test_db_command))
+    application.add_handler(MessageHandler(filters.Document.ALL, load_datafile_to_db))
 
     application.run_polling()
 
